574b8f423a7268ef563e5c423901d93e6bb96ba4
AndresM0112 <andresmurillo0112@gmail.com>
2026-03-06 09:27:29 -0500

    Refactor authAPI.js

--- client/src/api/requests/authAPI.js
+++ client/src/api/requests/authAPI.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const instance = axios.create({ baseURL: process.env.REACT_APP_API_URL || "" });
+const instance = axios.create({
+    baseURL: process.env.REACT_APP_API_URL || "",
+    withCredentials: true,
+});
 instance.CancelToken = axios.CancelToken;
 instance.isCancel = axios.isCancel;
 
