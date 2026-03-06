08abec30f02d361c0505dd0f7e58ac99eadec44d
AndresM0112 <andresmurillo0112@gmail.com>
2026-03-06 08:44:15 -0500

    connect back .env

--- client/src/api/requests/usersApi.js
+++ client/src/api/requests/usersApi.js
@@ -2,7 +2,7 @@ import Cookies from "js-cookie";
 import httpCliente from "../services/httpCliente";
 import axios from "axios";
 
-const instance = axios.create();
+const instance = axios.create({ baseURL: process.env.REACT_APP_API_URL || "" });
 instance.CancelToken = axios.CancelToken;
 instance.isCancel = axios.isCancel;
 
